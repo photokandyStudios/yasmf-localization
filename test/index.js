45e8d7cd1c9a12fd317fceeb6e87f0b40ead5e32
Kerri Shotts <kerrishotts@gmail.com>
2015-05-26 16:15:32 -0500

    test locale using locale2; test interpolation

--- test/index.js
+++ test/index.js
@@ -1,10 +1,46 @@
 "use strict";
+/* globals describe, it */
 
-let should = require("should");
+import Localization from "../src/index.js";
+let currentLocale = require("locale2");
+if (!currentLocale) { currentLocale = "en"; }
 
-describe("Test Category", () => {
-    describe("Test 1", () => {
-        it("should be something", () => {
+require("should");
+
+describe("Localization", () => {
+    describe("Loading Locale", () => {
+        let L = new Localization();
+        it("Should be " + currentLocale, (done) => {
+            L.loadLocale().then( (r) => {
+                r.should.be.equal(currentLocale);
+                done();
+            }).catch( (err) => done(err));
+        });
+    });
+    describe("Simple Translation", () => {
+        let L = new Localization();
+        it("Should return phrase for key", (done) => {
+            L.loadLocale().then( () => {
+                              L.loadTranslations(require("./localization/root/messages.json"));
+                              let lookup = L.T("key");
+
+                              lookup.should.be.equal("phrase");
+                              done();
+                          })
+             .catch( (err) => done(err));
+        });
+    });
+    describe("Interpolated Translation", () => {
+        let L = new Localization();
+        it("Should return hello, Kerri for key2", (done) => {
+            L.loadLocale().then( () => {
+                              L.loadTranslations(require("./localization/root/messages.json"));
+                              let lookup = L.T("key2", {name:"Kerri"});
+
+                              lookup.should.be.equal("hello, Kerri");
+                              done();
+                          })
+             .catch( (err) => done(err));
         });
     });
 });
